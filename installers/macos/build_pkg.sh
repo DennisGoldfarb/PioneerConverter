863d5e69d9f78b2d0403b2cae71994c11786fb18
Dennis Goldfarb <Goldfarb.dennis@gmail.com>
2025-07-15 16:30:04 -0500

    Update build_pkg.sh

--- installers/macos/build_pkg.sh
+++ installers/macos/build_pkg.sh
@@ -13,7 +13,7 @@ cp -R ../../dist/PioneerConverter-osx-x64/* "$PKGROOT/usr/local/$APPNAME/"
 
 cat <<'WRAP' > "$PKGROOT/usr/local/bin/pioneerconverter"
 #!/bin/bash
-/usr/local/$APPNAME/PioneerConverter/PioneerConverter "$@"
+/usr/local/$APPNAME/PioneerConverter "$@"
 WRAP
 chmod +x "$PKGROOT/usr/local/bin/pioneerconverter"
 
@@ -26,7 +26,7 @@ if [[ -n "$CODESIGN_IDENTITY" ]]; then
   echo "Codesigning binaries"
   codesign --verbose=4 --force --options runtime --timestamp \
     --sign "$CODESIGN_IDENTITY" \
-    "$PKGROOT/usr/local/$APPNAME/PioneerConverter/PioneerConverter"
+    "$PKGROOT/usr/local/$APPNAME/PioneerConverter"
 fi
 
 if [[ -n "$PKG_SIGN_IDENTITY" ]]; then
