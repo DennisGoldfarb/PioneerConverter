4df057eff76a1baa5077363fa33e13c804ebb99a
Dennis Goldfarb <Goldfarb.dennis@gmail.com>
2025-07-16 12:59:43 -0500

    Update build_installers.sh

--- build_installers.sh
+++ build_installers.sh
@@ -45,7 +45,8 @@ case "$TARGET" in
     windows)
         echo "Creating Windows installer"
         pushd installers/windows > /dev/null
-        iscc PioneerConverter.iss
+        echo $VERSION
+        iscc PioneerConverter.iss /DMyAppVersion=${VERSION}
         if [ -f Output/PioneerConverter-win-${VERSION}-Setup.exe ]; then
             mv "Output/PioneerConverter-win-${VERSION}-Setup.exe" "PioneerConverter-win-${VERSION}-Setup.exe"
         elif [ -f Output/PioneerConverter-win-Setup.exe ]; then
