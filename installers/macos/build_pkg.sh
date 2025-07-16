ae4d89b89c031d0785697669d6ef81021220bec7
Dennis Goldfarb <Goldfarb.dennis@gmail.com>
2025-07-15 20:25:44 -0500

    Update build_pkg.sh

--- installers/macos/build_pkg.sh
+++ installers/macos/build_pkg.sh
@@ -29,7 +29,7 @@ cp -R "$DIST"/* "$PKGROOT/usr/local/$APPNAME/"
 
 cat <<WRAP > "$PKGROOT/usr/local/bin/pioneerconverter"
 #!/bin/bash
-/usr/local/$APPNAME/PioneerConverter/PioneerConverter "\$@"
+/usr/local/$APPNAME/PioneerConverter/ "\$@"
 WRAP
 chmod +x "$PKGROOT/usr/local/bin/pioneerconverter"
 
